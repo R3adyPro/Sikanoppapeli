940effabc48492a719fa34715679909d361de1a5
R3adyPro <77439982+R3adyPro@users.noreply.github.com>
2022-04-05 12:29:50 +0300

    Add files via upload

--- pelaajan_valinta.js
+++ pelaajan_valinta.js
@@ -1,37 +1,49 @@
 const pelaajat = [];
 const pelaajaId = [];
 let maxPelaajaa = 4
+const pisteet = [0, 0, 0, 0]
+
+
 
 id = 1;
 max = 0;
+peliMuoto = 0;
 noppa = 0;
 
-function testi(){
-    console.log(pelaajaId)
-    console.log(pelaajat)
-    console.log(noppa)
+
+function noppanNumero(){
+    noppa = Math.floor(Math.random() * 6) + 1;
 }
 
+
+//Yhden nopan pelimuoto
 function Pelaa(){
+    noppanNumero();
+    document.getElementById('noppa').innerHTML = noppa;
+}
+
+
+//Pelaajien valinta ja pelimuodon valinta
+function Pelaajat(){
+    p = 1;
     for(i=0; i<pelaajat.length; i++){
-        document.getElementById('pelaajat' + i).innerHTML = 'ds';
+        document.getElementById('pelaajat' + p).innerHTML = pelaajat[i];
+        document.getElementById('pisteet' + p).innerHTML = pisteet[i];
+        p++;
     }
-    console.log(1)
+    id = 1;
 }
 
 function lisaaPelaaja(){
     var pelaajanNimi = document.getElementById('nimi').value;
     var onkoTyhja = pelaajanNimi.length;
     const pelaajaIso = pelaajanNimi.charAt(0).toUpperCase() + pelaajanNimi.slice(1);
-    console.log(pelaajaId)
-    console.log(pelaajat)
     if(onkoTyhja > 1 && max < maxPelaajaa){
         pelaajat.push(pelaajaIso);
         pelaajaId.push(id)
         document.getElementById('pelaaja' + id).innerHTML = id + '. ' + pelaajaIso;
         id = id + 1;
         max = max + 1;
-        console.log(noppa)
     }
     else if(maxPelaajaa == 4){
         alert('Pelaaja raja saavutettu');
@@ -39,9 +51,9 @@ function lisaaPelaaja(){
 }
 
 function yksNoppa(){
-    noppa = 1;
+    peliMuoto = 1;
 }
 function kaksNoppaa(){
-    noppa = 2;
+    peliMuoto = 2;
 }
 
